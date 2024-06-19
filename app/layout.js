82196b200e8e4e5ffe4e339271c61799a828d282
Elijah Gaytan <elijahgaytan@utexas.edu>
2024-06-18 22:36:29 -0500

    routes

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+// cool syntax @ starts from root
+// import Logo from "@/app/_components/Logo";
+// import Navigation from "./_components/Navigation";
+
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: { template: "%s - Oasis", default: "Welcome - Oasis" },
+  description:
+    "Luxurious cabin hotel booking service. Find you dream hotel, and your ideal solitutde",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} relative antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
