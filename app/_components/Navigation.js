7f417dc89a5e09a06f461238cff8834cc3a673c8
Elijah Gaytan <elijahgaytan@utexas.edu>
2024-06-21 01:09:44 -0500

    small change

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -43,7 +43,7 @@ export default async function Navigation() {
               href="/account"
               className="hover:text-accent-400 transition-colors"
             >
-              Guest area
+              Login
             </Link>
           )}
         </li>
